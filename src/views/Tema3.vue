450da63eba931f95519f7a0175d5068b777df21e
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-05-12 13:07:23 -0500

    Tema tres listo

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -1,12 +1,92 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.bg-tema3
     .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
+      .titulo-principal__numero.text-white
         span 3
-      h1 Titulo de primer nivel
-
+      h1 Protocolos y manuales técnicos de la informática
+    .row.mb-justify-content-center.mb-4
+        .col-lg-7
+          .d-flex.align-items-center.justify-content-center.mb-3
+            img(src="@/assets/curso/temas/53.svg" style="width: 85px").me-3.d-lg-block.d-none
+            p En las organizaciones, al momento de aplicar o hacer uso de cualquier herramienta ofimática, informática o digital, se establecen unos manuales para su adecuado manejo, con miras a cubrir alguna necesidad o dar respuesta a la solución de alguna situación imprevista. 
+          p También se crean políticas y reglamentos al momento del uso de estos instrumentos, con lo que se evita el inadecuado manejo de las mismas y su autorización.
+            br
+            br
+            | Por otro lado, se establece la información técnica de cada una con el propósito de saber sobre su utilidad, licenciamientos y en ocasiones vida útil de los productos y soporte técnico, respetando las normas y derechos de autor de cada una.
+        .col-lg-5: img(data-aos='zoom-in-up')(src="@/assets/curso/temas/54.png")
+    #t_3_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2.mb-1 3.1  Políticas
+    .d-flex.align-items-center.justify-content-center.mb-5
+      img(src="@/assets/curso/temas/35.svg" style="width: 85px").me-3.d-lg-block.d-none
+      p Estas se crean con el propósito de salvaguardar la información, privacidad y confidencialidad, generando un buen uso de las herramientas disponibles en la organización. Cabe resaltar que las políticas que se crean son de carácter obligatorio en su cumplimiento y el no acatarlas generan consecuencias sancionatorias; por ello es importante saber qué tipos de políticas se emplean en las organizaciones para cumplir lo anteriormente visto.
+    p.text-center 
+      b Estas políticas pueden ser:
+    .row.justify-content-center.mb-4
+      .col-lg-6.mb-3.mb-lg-0
+        .tarjeta-up
+          img(src="@/assets/curso/temas/55.jpg")
+          .tarjeta-up__content.color-primario.p-5
+            h3.mb-4 Políticas de manejo 
+            p Enfocadas en las buenas prácticas que todos los empleados o usuarios deben compartir en el momento de hacer uso de las herramientas.
+      .col-lg-6
+        .tarjeta-up
+          img(src="@/assets/curso/temas/56.jpg")
+          .tarjeta-up__content.color-acento-contenido.p-5
+            h3.mb-4 Políticas de seguridad y riesgos
+            p.mb-5 Diseñadas para mitigar los riesgos que se originan, en ocasiones, por la inocencia del personal, como compartir contraseñas, descargar de anuncios, programas y demás situaciones que pueden generar tanto virus, robo de información y perjuicios económicos.
+    p.mb-5 Se debe dar importancia y estricto cumplimiento a las políticas establecidas en cada organización para mitigar situaciones de riesgo. 
+    Separador
+    #t_3_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 3.2  Taxonomía y contenido del manual
+    .d-flex.align-items-start.justify-content-center.mb-3
+      img(src="@/assets/curso/temas/57.svg" style="width: 85px").me-3.d-lg-block.d-none
+      p Entendiendo que los protocolos y manuales se crean para guiar al usuario en el manejo y buen uso de las herramientas ofimáticas, informáticas y digitales, se deben tener en cuenta para su creación los siguientes aspectos:
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 1.
+          span Aspectos para los protocolos y manuales
+        img(src="@/assets/curso/temas/58.svg")
+    p.mb-5 Teniendo en cuenta los aspectos anteriores, se indica el contenido del manual de las herramientas informáticas, digitales y ofimáticas de la siguiente manera:
+    .row.justify-content-center.mb-5
+      .col-lg-4.d-none.d-lg-block: img(data-aos='zoom-in')(src="@/assets/curso/temas/59.svg")
+      .col-lg-8
+        ul.lista-ul--color
+          li
+            i.dot-circle-regular
+            | Se establece un objetivo del manual para que el usuario entienda la importancia del mismo y aplique su responsable uso.
+          li
+            i.dot-circle-regular
+            | Se parametriza el contenido para que sea fácil encontrar la información al momento de consultarlo.
+          li
+            i.dot-circle-regular
+            | Se genera la cobertura o alcance de quiénes pueden acceder o ver la información de cada una de los instrumentos.
+          li
+            i.dot-circle-regular
+            | Se presentan las funciones y ventajas de cada herramienta, así como requisitos de instalación, capacidades, accesos, permisos y demás requisitos.
+          li
+            i.dot-circle-regular
+            | Datos de instalación, permisos y configuración para descargar de forma correcta según cada equipo.
+          li
+            i.dot-circle-regular
+            | Funciones generales y específicas de acuerdo con los permisos que tenga cada usuario al acceder para la realización de las tareas.
+          li
+            i.dot-circle-regular
+            | Se plasma la información, telefónica, en la red y física de soporte técnico para dar respuesta oportuna a los requerimientos de los usuarios y no detener la operación.
+          li
+            i.dot-circle-regular
+            | Políticas de privacidad y seguridad informática para evitar pérdida o fuga de información y salvaguardarla.
+          li
+            i.dot-circle-regular
+            | Se genera un glosario con palabras clave que puedan apoyar en la comprensión y gestión del uso de las herramientas establecidas.
+    p.mb-5 Esta es la información básica y de contenido que deben contener los manuales, y según su necesidad las organizaciones incluyen ítems de confidencialidad, prohibición, cláusulas de manejo y demás que sean asignadas por el tipo de licencia que se adquiere en la empresa.
+    .row.justify-content-center.mb-5
+      .col-lg-9
+        .cajon.color-acento-contenido.morado-claro.p-4.mb-4
+          p.mb-0 Es importante reconocer y adaptarse a las normas establecidas en las organizaciones y no se puede hacer descargas de herramientas ofimáticas, informáticas y digitales sin una autorización, porque esto puede traer sanciones tanto al funcionario como a la empresa por temas de licenciamientos o por fugas de contenidos confidenciales por violación a la seguridad de la información.
+      .col-lg-3: img(data-aos='zoom-in-up')(src="@/assets/curso/temas/60.svg")
 </template>
 
 <script>
@@ -26,4 +106,14 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.bg-tema3
+  background-image: url('../assets/curso/temas/bg-tema3.svg')
+  background-size: cover
+  background-position: start
+.dot-circle-regular
+  &:before
+    content: url('../assets/curso/temas/dot-circle-regular.svg')
+.morado-claro
+  background-color: #ede7f7 !important
+</style>
