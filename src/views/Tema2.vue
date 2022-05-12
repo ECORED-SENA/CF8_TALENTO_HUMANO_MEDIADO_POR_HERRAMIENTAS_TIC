3d0a81f4b8495359c4c28d1b9f2b79c681d11cf5
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-05-12 07:57:07 -0500

    Tema 2 listo

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,169 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.bg-tema2
     .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
+      .titulo-principal__numero.text-white
         span 2
-      h1 Titulo de primer nivel
+      h1 Herramientas ofimáticas
+    .row.justify-content-center.align-items-start.mb-5
+      .col-lg-4
+        p Con los instrumentos digitales e informáticos estudiados, en donde se almacenan y comparten diferentes tipos de archivos creados con las herramientas ofimáticas, se complementa la gestión que hacen los usuarios en las organizaciones dependiendo de su responsabilidad o actividades de cada área.
+      .col-lg-3: img(data-aos='zoom-in')(src="@/assets/curso/temas/34.svg")
+      .col-lg-5
+        
+            img(src="@/assets/curso/temas/35.svg" style="width: 84px").me-3.d-lg-block.d-none.mb-3
+            p La ofimática es un instrumento creado para ejercer las actividades comunes en las organizaciones tales como informes, bases de datos, presentaciones, programaciones, indicadores de gestión. Es decir, es una herramienta creada para mejorar la calidad de vida de los trabajadores en las oficinas y como su nombre lo indica ofi-matica es la unión de las palabras oficina e informática.
+    #t_2_1.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 2.1  Tipos
+    p.mb-5 Dentro de las herramientas ofimáticas, existen unas que son más utilizadas por las organizaciones para mejorar el desempeño y productividad de sus actividades diarias. Estas son:
+    .row.justify-content-center.align-items-start.mb-
+      .col-lg-5: img(data-aos='zoom-in')(src="@/assets/curso/temas/36.svg")
+      .col-lg-7
+        SlyderF.mb-5.aling-cards(columnas="col-lg-4 col-xl-4")
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/37.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Access
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/38.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Word
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/39.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Power Point
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/40.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Excel
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/41.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Outlook, Gmail, Yahoo 
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/42.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Publisher 
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/43.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Paint
+          .tarjeta.hover-morado.p-4.py-5.h-100
+            img(src="@/assets/curso/temas/44.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Herramientas en la nube de Google, Outlook
+    Separador
+    #t_2_2.titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 2.2  Descripción y funcionalidad
+    .d-flex.align-items-center.justify-content-center.mb-3
+      img(src="@/assets/curso/temas/3.svg" style="width: 84px").me-3.d-lg-block.d-none
+      p Sin duda, las herramientas ofimáticas ofrecen múltiples servicios en los diferentes contextos en los que se emplean, debido a que estas cobran importancia porque se basan en la creación, manipulación, transmisión y almacenamiento de la información que se genera.
+    p.mb-5 Ahora, se invita a conocer la descripción de cada una de las herramientas mencionadas anteriormente:
+    .row.justify-content-center.mb-3
+      .col-lg-3
+        .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/37.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Access
+            p.text-center Herramienta para administrar bases de datos.
+      .col-lg-3
+          .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/38.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Word
+            p.text-center Herramienta para procesar textos.
+      .col-lg-3
+          .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/39.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Power Point
+            p.text-center Herramienta para presentaciones
+      .col-lg-3
+          .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/40.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Excel
+            p.text-center Herramienta para crear base de datos, gráficos, tablas, en la cual se pueden desarrollar diferentes operaciones lógicas.
+    .row.justify-content-center.mb-5
+      .col-lg-3
+          .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/41.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Outlook, Gmail, Yahoo
+            p.text-center  Herramientas para enviar y recibir mensajes por medio de mails.
+      .col-lg-3
+          .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/42.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Publisher 
+            p.text-center Herramienta de diseño.
+      .col-lg-3
+          .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/43.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Paint
+            p.text-center Herramienta para edición de gráficos.
+      .col-lg-3
+          .tarjeta.hover-morado2.p-4.py-5.h-100(data-aos='flip-down')
+            img(src="@/assets/curso/temas/44.png" style=' width: 93px').mb-3.m-auto
+            p.text-center
+              b Herramientas en la nube de Google, Outlook
+            p.text-center Herramientas en la red para trabajar diferentes actividades de oficina en línea. 
+    p.text-center
+      span.b--3 Y sus funcionalidades son:
+    .slyder-1.px-5.py-3.mb-5
+      SlyderA(tipo='b')
+        .row.justify-content-center.align-items-center
+          .col-lg-6
+            p
+              span.b--3 Access
+            p Sirve para gestionar las bases de datos y hacer valoración de clientes, proveedores, compras, pedidos, trabajadores y demás datos que se requieran para generar informes o, simplemente, para la toma de decisiones.
+          .col-lg-5: img(src="@/assets/curso/temas/45.png")
+        .row.justify-content-center.align-items-center
+          .col-lg-6
+            p
+              span.b--3 Word
+            p Se utiliza para crear y modificar textos elaborando documentos como cartas, informes, presentaciones, hojas de vida y demás requeridos.
+          .col-lg-5: img(src="@/assets/curso/temas/46.png")
+        .row.justify-content-center.align-items-center
+          .col-lg-6
+            p
+              span.b--3 Power Point 
+            p Herramienta para crear presentaciones para reuniones, capacitaciones, informes o según la necesidad del funcionario en la empresa.
+          .col-lg-5: img(src="@/assets/curso/temas/47.png")
+        .row.justify-content-center.align-items-center
+          .col-lg-6
+            p
+              span.b--3 Excel
+            p Se usa para funciones matemáticas, bases de datos, parametrizar información, creación de gráficos, planeación y demás informes que se requieran.
+          .col-lg-5: img(src="@/assets/curso/temas/48.png")
+        .row.justify-content-center.align-items-center
+          .col-lg-6
+            p
+              span.b--3 Outlook, Gmail, Yahoo
+            p Desde estas herramientas se envían correos, se adjuntan archivos y se pueden parametrizar reuniones. Se debe tener en cuenta que es un canal de comunicación interno y externo de las organizaciones.
+          .col-lg-5: img(src="@/assets/curso/temas/49.png")
+        .row.justify-content-center.align-items-center
+          .col-lg-6
+            p
+              span.b--3 Publisher
+            p Es ideal para crear folletos, presentaciones, diagramas y demás formatos para publicidad.
+          .col-lg-5: img(src="@/assets/curso/temas/50.png")
+        .row.justify-content-center.align-items-center
+          .col-lg-6
+            p
+              span.b--3 Paint
+            p Sirve para gestionar, crear y modificar gráficos en la organización, es muy útil cuando se envían mensajes de motivación al personal como celebraciones de cumpleaños, reconocimientos a la labor y demás.
+          .col-lg-5: img(src="@/assets/curso/temas/51.png")
+    .row.justify-content-center.mb-2.g-5
+      .col-lg-7
+        p Es importante recordar que las herramientas se pueden guardar en la nube o compartir según las aplicaciones existentes.
+        .card-icon.color-neutral.p-4
+          p.mb-0 En la red también se pueden crear y gestionar los diferentes tipos de documentos antes vistos por medio de Google, Yahoo y Outlook. Se invita a que, desde su usuario de correo electrónico SENA o personal, revise cada una de estas herramientas.
+          .card-icon__img
+            img(src="@/assets/curso/temas/5.svg")
+      .col-lg-5: img(data-aos='zoom-in-up')(src="@/assets/curso/temas/52.svg")
 
 </template>
 
@@ -26,4 +184,12 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.bg-tema2
+  background-image: url('../assets/curso/temas/bg-tema2.png')
+  background-size: cover
+  background-position: start
+.aling-cards
+  .horizontal-scroll
+    align-items: start !important
+</style>
